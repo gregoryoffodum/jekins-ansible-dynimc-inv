38b0f43235ef459f5ac28b84680c41e7ab7e65f8
Gregory Offodum <78843321+gregoryoffodum@users.noreply.github.com>
2023-03-21 07:43:36 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,14 +1,14 @@
-provider 'aws' {
-region='us-east-2'
+provider "aws" {
+region="us-east-2"
 }
 
-resource 'aws instance' 'AWSInstance'{
-ami='ami-0521a1ab6cb98215d'
-instance_type='t2-micro'
-key_name='devops'
-security_groups=['launch-wizard-1']
+resource "aws instance" "AWSInstance"{
+ami="ami-0521a1ab6cb98215d"
+instance_type="t2-micro"
+key_name="devops"
+security_groups=["launch-wizard-1"]
 tags={
-Name='tomcatservers'
+Name="tomcatservers"
 }
 
 }
