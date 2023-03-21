68c180c28d1bb4e4909025ae6db08889d32e4ba3
Gregory Offodum <78843321+gregoryoffodum@users.noreply.github.com>
2023-03-21 07:46:57 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ region="us-east-2"
 
 resource "aws_instance" "AWSInstance"{
 ami="ami-0521a1ab6cb98215d"
-instance_type="t2-micro"
+instance_type="t2.micro"
 key_name="devops"
 security_groups=["launch-wizard-1"]
 tags={
